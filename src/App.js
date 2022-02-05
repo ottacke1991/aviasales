90ad7c7d1efacd93adaefb898af5655908f2aecf
aleksandrkondratev <ottacke1991@gmail.com>
2022-02-05 19:36:45 +0400

    Реализовал позиционирование основных блоков, CSS именование классов (как на оригинальном aviaSales).

--- src/App.js
+++ src/App.js
@@ -4,9 +4,24 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-
-      </header>
+      <div className="app__container">
+        <div className="app__header">
+            <img src={logo}/>
+        </div>
+        <div className="app__main">
+          <div className="app__sidebar">
+              1
+          </div>
+          <div className="app__content">
+              <div className="ticket-option-group">
+                  1
+              </div>
+              <div className="product-list">
+                  <div className="product-list__item">1</div>
+              </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
